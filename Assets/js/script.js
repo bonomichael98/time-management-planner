9d239fddbc460b8669e2d9d15cfa3cfd361b4642
michael <shinlin09@gmail.com>
2022-03-26 12:10:36 -0400

    adding elements to page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,19 +8,18 @@ let timeOfTasks = document.getElementsByClassName("time");
 document.getElementById("currentDay").innerHTML = date;
 
 //timeblocks selected
-let nine = document.getElementById("9");
-let ten = document.getElementById("10");
-let eleven = document.getElementById("11");
-let twelve = document.getElementById("12");
-let onePm = document.getElementById("13");
-let twoPm = document.getElementById("14");
-let threePm = document.getElementById("15");
-let fourPm = document.getElementById("16");
-let fivePm = document.getElementById("17");
+let nine = document.getElementById("input-9");
+let ten = document.getElementById("input-10");
+let eleven = document.getElementById("input-11");
+let twelve = document.getElementById("input-12");
+let onePm = document.getElementById("input-13");
+let twoPm = document.getElementById("input-14");
+let threePm = document.getElementById("input-15");
+let fourPm = document.getElementById("input-16");
+let fivePm = document.getElementById("input-17");
 
 
 
-//pageDate.innerHTML = 'date';
 
 //date.toString().innerHTML = pageDate;
 
@@ -36,9 +35,9 @@ $(document).ready(function(){
     //button click to save to local storage
     $("button").click(function() {
         //set input text as variable
-        let description = $(this).parent("div").prev("div").children("input-text").val();        
-        let taskTime = $(this).parent("div").prev("id").val();
-        localStorage.setItem(description, taskTime);
+        let description = $(this).parent("div").prev().children(".input-text").val();
+        let taskTime = $(this).parent("div").prev().attr("id");
+        localStorage.setItem(taskTime, description);
     })
 
 
@@ -48,14 +47,36 @@ $(document).ready(function(){
 
 });
 
-// let startPage = function() {
-//     let addNine = JSON.parse(localStorage.getItem())
-//     let addTen = JSON.parse(localStorage.getItem())
-//     let addEleven = JSON.parse(localStorage.getItem())
-//     let addTwelve = JSON.parse(localStorage.getItem())
-//     let addOnePm = JSON.parse(localStorage.getItem())
-//     let addTwoPm = JSON.parse(localStorage.getItem())
-//     let addThreePm = JSON.parse(localStorage.getItem())
-//     let addFourPm = JSON.parse(localStorage.getItem())
-//     let addFivePm = JSON.parse(localStorage.getItem())
-// }
+//get local storage elements and display them to the page
+let startPage = function() {
+    let addNine = JSON.parse(localStorage.getItem("9"));
+    nine.val(addNine);
+    console.log(addNine);
+
+    let addTen = JSON.parse(localStorage.getItem("10"));
+    ten.val(addTen);
+
+    let addEleven = JSON.parse(localStorage.getItem("11"));
+    eleven.val(addEleven);
+
+    let addTwelve = JSON.parse(localStorage.getItem("12"));
+    twelve.val(addTwelve);
+
+    let addOnePm = JSON.parse(localStorage.getItem("13"));
+    onePm.val(addOnePm);
+
+    let addTwoPm = JSON.parse(localStorage.getItem("14"));
+    twoPm.val(addTwoPm);
+
+    let addThreePm = JSON.parse(localStorage.getItem("15"));
+    threePm.val(addThreePm);
+
+    let addFourPm = JSON.parse(localStorage.getItem("16"));
+    fourPm.val(addFourPm);
+
+    let addFivePm = JSON.parse(localStorage.getItem("17"));
+    fivePm.val(addFivePm);
+
+}
+
+startPage();
