167771c5cc745673c812ef2a20d85ed67c960c34
michael <shinlin09@gmail.com>
2022-03-26 16:14:58 -0400

    finished

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -18,7 +18,7 @@ let threePm = document.getElementById("input-15");
 let fourPm = document.getElementById("input-16");
 let fivePm = document.getElementById("input-17");
 
-
+let timeId = document.getElementsByClassName("time");
 
 
 //date.toString().innerHTML = pageDate;
@@ -38,45 +38,87 @@ $(document).ready(function(){
         let description = $(this).parent("div").prev().children(".input-text").val();
         let taskTime = $(this).parent("div").prev().attr("id");
         localStorage.setItem(taskTime, description);
+
+        
     })
+    $(".time").each(function () {
+        let itemTime = $(this).attr("id");
+        console.log(itemTime)
+        if (itemTime < time) {
+            $(this).addClass("bg-danger");
+        } else if(itemTime > time) {
+            $(this).css("background", "green");
+
+
+        } else {
+            $(this).css("background", "grey");
+
+        }    
+    })
+
+
+    let timeBg = function(timeId, time) {
+            
+    }
+
+timeBg()
+    //set background colors
+    // if (timeBackground < time) {
+    //     $(".time").each(function() {
+    //         let currentBlock = $(".time").attr("id");
+    //         let timeBackground = $(".time").attr("id");
+    //         timeBackground.addClass("bg-danger");
+    //         })
+    //     }
+    // })
+    
+    // if (taskTime < time) {
+    //     //let taskTime = $(this).parent("div").prev().attr("id");
+
+    //     let background = $(this).parent().prev().children("textArea");
+    //     background.addClass("bg-danger");
+    // }
 
 
     //for
 
     //if(time < )
 
-});
+    //get local storage elements and display them to the page
+    
+    let startPage = function() {
+        let addNine = localStorage.getItem("9");
+        nine.textContent= addNine;
 
-//get local storage elements and display them to the page
-let startPage = function() {
-    let addNine = JSON.parse(localStorage.getItem("9"));
-    nine.val(addNine);
-    console.log(addNine);
+        let addTen = localStorage.getItem("10");
+        ten.textContent = addTen;
 
-    let addTen = JSON.parse(localStorage.getItem("10"));
-    ten.val(addTen);
+        let addEleven = localStorage.getItem("11");
+        eleven.textContent = addEleven;
 
-    let addEleven = JSON.parse(localStorage.getItem("11"));
-    eleven.val(addEleven);
+        let addTwelve = localStorage.getItem("12");
+        twelve.textContent  = addTwelve;
 
-    let addTwelve = JSON.parse(localStorage.getItem("12"));
-    twelve.val(addTwelve);
+        let addOnePm = localStorage.getItem("13");
+        onePm.textContent  = addOnePm;
 
-    let addOnePm = JSON.parse(localStorage.getItem("13"));
-    onePm.val(addOnePm);
+        let addTwoPm = localStorage.getItem("14");
+        twoPm.textContent  = addTwoPm;
 
-    let addTwoPm = JSON.parse(localStorage.getItem("14"));
-    twoPm.val(addTwoPm);
+        let addThreePm = localStorage.getItem("15");
+        threePm.textContent = addThreePm;
 
-    let addThreePm = JSON.parse(localStorage.getItem("15"));
-    threePm.val(addThreePm);
+        let addFourPm = localStorage.getItem("16");
+        fourPm.textContent = addFourPm;
 
-    let addFourPm = JSON.parse(localStorage.getItem("16"));
-    fourPm.val(addFourPm);
+        let addFivePm = localStorage.getItem("17");
+        fivePm.textContent  = addFivePm;
 
-    let addFivePm = JSON.parse(localStorage.getItem("17"));
-    fivePm.val(addFivePm);
+    }
+    startPage();
 
-}
+})
 
-startPage();
+
+
+
